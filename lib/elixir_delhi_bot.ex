307fce306786e3f27015f3df5e0aabed91101224
Animesh-Ghosh <animesh.ghosh@tuta.io>
2024-03-30 17:11:18 +0530

    Create ElixirDelhiBot.Worker module
    Create ElixirDelhiBot.greet_new_members/1 function.
    Add ElixirDelhiBot.Worker to the application's supervision tree.

--- lib/elixir_delhi_bot.ex
+++ lib/elixir_delhi_bot.ex
@@ -1,18 +1,73 @@
 defmodule ElixirDelhiBot do
-  @moduledoc """
-  Documentation for `ElixirDelhiBot`.
-  """
+  def greet_new_members(last_update_id) do
+    # TODO: too many helper methods, move into a separate module
+    with {:ok, updates} <- get_updates(),
+         {:ok, chat_id, new_chat_members, update_id} <-
+           new_chat_members_from(updates, last_update_id) do
+      greet_new_members(chat_id, new_chat_members)
+      {:ok, update_id}
+    end
+  end
 
-  @doc """
-  Hello world.
+  defp get_updates do
+    {:ok, %{"result" => result}} = ElixirDelhiBot.Telegramex.get_updates()
+    {:ok, result}
+  end
 
-  ## Examples
+  defp new_chat_members_from(updates, last_update_id) do
+    {:ok, chat_id, new_chat_members, update_id} = last_update_of(updates)
 
-      iex> ElixirDelhiBot.hello()
-      :world
+    new_chat_members =
+      if new_update?(last_update_id, update_id) do
+        new_chat_members
+      else
+        []
+      end
 
-  """
-  def hello do
-    :world
+    {:ok, chat_id, new_chat_members, update_id}
+  end
+
+  defp last_update_of(updates) do
+    %{
+      "message" => %{
+        "chat" => %{
+          "id" => chat_id
+        },
+        "new_chat_members" => new_chat_members
+      },
+      "update_id" => update_id
+    } = List.last(updates)
+
+    {:ok, chat_id, new_chat_members, update_id}
+  end
+
+  defp new_update?(last_update_id, current_update_id) do
+    if is_nil(last_update_id) do
+      true
+    else
+      current_update_id > last_update_id
+    end
+  end
+
+  defp greet_new_members(_chat_id, new_chat_members) when length(new_chat_members) == 0, do: :noop
+
+  defp greet_new_members(chat_id, new_chat_members) do
+    non_bot_members(new_chat_members)
+    |> Enum.each(fn new_chat_member ->
+      greet_new_member(chat_id, new_chat_member)
+    end)
+  end
+
+  def greet_new_member(chat_id, new_chat_member) do
+    ElixirDelhiBot.Telegramex.send_message(
+      chat_id,
+      "A wild #{new_chat_member["first_name"]} appeared!"
+    )
+  end
+
+  defp non_bot_members(new_chat_members) do
+    Enum.filter(new_chat_members, fn new_chat_member ->
+      !new_chat_member["is_bot"]
+    end)
   end
 end
