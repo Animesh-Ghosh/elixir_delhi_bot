7832726fbbfb0fc4c8fa7e91933f04baf00611bf
Animesh-Ghosh <animesh.ghosh@tuta.io>
2024-05-14 00:48:16 +0530

    Refactor code to model concerns properly
    Poller polls updates and the bot processes them.

--- lib/elixir_delhi_bot.ex
+++ lib/elixir_delhi_bot.ex
@@ -1,4 +1,44 @@
 defmodule ElixirDelhiBot do
+  def process_updates([], last_update_id), do: {:ok, last_update_id}
+
+  def process_updates(updates, last_update_id) do
+    case first_unprocessed_update(updates, last_update_id) do
+      {:error, :no_unprocessed_update} ->
+        {:ok, last_update_id}
+
+      {:ok, update} ->
+        process_update(update)
+        {:ok, update["update_id"]}
+    end
+  end
+
+  defp first_unprocessed_update(updates, nil), do: List.last(updates)
+
+  defp first_unprocessed_update(updates, last_update_id) do
+    first_unprocessed_update =
+      Enum.find(updates, fn update -> update["update_id"] > last_update_id end)
+
+    case first_unprocessed_update do
+      nil -> {:error, :no_unprocessed_update}
+      update -> {:ok, update}
+    end
+  end
+
+  def process_update(%{} = update) do
+    case update do
+      %{
+        "message" => %{
+          "chat" => %{"id" => chat_id},
+          "new_chat_members" => new_chat_members
+        }
+      } ->
+        greet_new_members(chat_id, new_chat_members)
+
+      _ ->
+        {:error, :unhandled_update}
+    end
+  end
+
   def greet_new_members(last_update_id) do
     # TODO: too many helper methods, move into a separate module
     with {:ok, updates} <- get_updates(),
