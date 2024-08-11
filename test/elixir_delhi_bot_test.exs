1c37350490cc22858aeff3d36fdbfe2d8209f8d2
Animesh-Ghosh <animesh.ghosh@tuta.io>
2024-08-11 17:39:00 +0530

    feat: Add tests for the ElixirDelhiBot module

--- test/elixir_delhi_bot_test.exs
+++ test/elixir_delhi_bot_test.exs
@@ -2,7 +2,71 @@ defmodule ElixirDelhiBotTest do
   use ExUnit.Case, async: true
   doctest ElixirDelhiBot
 
-  test "greets the world" do
-    assert ElixirDelhiBot.hello() == :world
+  import Mox
+
+  setup :verify_on_exit!
+
+  describe "process_updates/2" do
+    test "returns the same update_id if no updates are present" do
+      last_update_id = 1
+      assert {:ok, last_update_id} == ElixirDelhiBot.process_updates([], last_update_id)
+    end
+
+    test "returns the same update_id if no new updates are present" do
+      last_update_id = 1
+      updates = [%{"update_id" => 1}]
+      assert {:ok, last_update_id} == ElixirDelhiBot.process_updates(updates, last_update_id)
+    end
+
+    test "returns the latest update_id if no update_id is present" do
+      latest_update_id = 1
+      updates = [%{"update_id" => latest_update_id}]
+      assert {:ok, latest_update_id} == ElixirDelhiBot.process_updates(updates, nil)
+    end
+
+    test "returns the new update_id if a newer update was handled" do
+      last_update_id = 1
+      new_update_id = 2
+
+      updates = [
+        %{
+          "update_id" => new_update_id,
+          "message" => %{
+            "chat" => %{"id" => 1},
+            "new_chat_members" => [%{"is_bot" => false, "first_name" => "John"}]
+          }
+        }
+      ]
+
+      expect(ElixirDelhiBot.TelegramexMock, :send_message, 1, fn _chat_id, _text ->
+        %{}
+      end)
+
+      assert {:ok, new_update_id} == ElixirDelhiBot.process_updates(updates, last_update_id)
+    end
+  end
+
+  describe "process_update/1" do
+    test "returns an error tuple for an unhandled update" do
+      assert {:error, :unhandled_update} == ElixirDelhiBot.process_update(%{})
+    end
+
+    test "processes new chat members update" do
+      new_update_id = 2
+
+      update = %{
+        "update_id" => new_update_id,
+        "message" => %{
+          "chat" => %{"id" => 1},
+          "new_chat_members" => [%{"is_bot" => false, "first_name" => "John"}]
+        }
+      }
+
+      expect(ElixirDelhiBot.TelegramexMock, :send_message, 1, fn _chat_id, _text ->
+        %{}
+      end)
+
+      assert :noop == ElixirDelhiBot.process_update(update)
+    end
   end
 end
