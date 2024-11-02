35d9bddb58b36a2911d77122a926e6f30999743c
Animesh-Ghosh <animesh.ghosh@tuta.io>
2024-11-02 06:58:58 +0530

    feat: Support /help command

--- test/elixir_delhi_bot/executor_test.exs
+++ test/elixir_delhi_bot/executor_test.exs
@@ -6,13 +6,26 @@ defmodule ElixirDelhiBot.ExecutorTest do
   setup :verify_on_exit!
 
   describe "bot_command?/1" do
-    test "returns true when a bot command is received" do
+    test "returns true when a /start bot command is received" do
       update = %{
         "update_id" => 1,
         "message" => %{
           "chat" => %{"id" => 1},
           "entities" => [%{"type" => "bot_command"}],
           "text" => "/start"
+        }
+      }
+
+      assert ElixirDelhiBot.Executor.bot_command?(update)
+    end
+
+    test "returns true when a /help bot command is received" do
+      update = %{
+        "update_id" => 1,
+        "message" => %{
+          "chat" => %{"id" => 1},
+          "entities" => [%{"type" => "bot_command"}],
+          "text" => "/help"
         }
       }
 
@@ -49,6 +62,28 @@ defmodule ElixirDelhiBot.ExecutorTest do
             %{"type" => "bot_command"}
           ],
           "text" => "/start"
+        }
+      }
+
+      ElixirDelhiBot.Executor.handle_bot_command(update)
+    end
+
+    test "sends the help message for the /help command" do
+      expected_chat_id = 1
+
+      expect(ElixirDelhiBot.TelegramexMock, :send_message, 1, fn chat_id, _text ->
+        assert chat_id == expected_chat_id
+        %{}
+      end)
+
+      update = %{
+        "update_id" => 1,
+        "message" => %{
+          "chat" => %{"id" => expected_chat_id},
+          "entities" => [
+            %{"type" => "bot_command"}
+          ],
+          "text" => "/help"
         }
       }
 
